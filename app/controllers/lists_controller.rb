94ff77ca97df52e946466e874633bd9e0482bfd7
Mathieu GUILLOT <mathieu.guillot@gaminglink.fr>
2023-05-26 15:38:20 +0200

    try again

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,14 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: %i[show edit update destroy]
+  before_action :set_list, only: [:show, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
+  end
 
   def new
     @list = List.new
@@ -8,40 +17,24 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      @list.save
       redirect_to list_path(@list)
     else
-      redirect_to new_list_path(@list)
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def index
-    @lists = List.all
-  end
-
-  def show; end
-
-  def edit; end
-
-  def update
-    @list.update(list_params)
-    redirect_to list_path(@list)
-  end
-
   def destroy
-    bookmarks = @list.bookmarks
-    bookmarks.each(&:destroy)
     @list.destroy
     redirect_to lists_path, status: :see_other
   end
 
   private
 
-  def list_params
-    params.require(:list).permit(:name)
-  end
-
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
   end
 end
